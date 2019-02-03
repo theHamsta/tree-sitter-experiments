67bc648f9342ecc9b1ac0a7b1e88e7c50b89bf79
Stephan Seitz <stephan.seitz@fau.de>
2019-02-03 15:40:01 +0100

    Parse cpp Code

--- main.cpp
+++ main.cpp
@@ -16,10 +16,28 @@ int main() {
   TSParser *parser = ts_parser_new();
 
   // Set the parser's language (JSON in this case).
-  ts_parser_set_language(parser, tree_sitter_json());
+  ts_parser_set_language(parser, tree_sitter_cpp());
 
   // Build a syntax tree based on source code stored in a string.
-  const char *source_code = "[1, null]";
+  const char *source_code = 
+R"(      
+#include <iostream>
+#include <exception>
+
+#include <opencv2/core.hpp>
+
+
+using namespace std;
+
+
+int main( int argc, const char** argv )
+{
+    cout << "Hallo Welt!" << endl;
+	cv::Mat a;
+	a.create(3, 3, CV_32FC1);
+	cout << a.at<float>(1,1) << endl;
+}
+)";
   TSTree *tree =
       ts_parser_parse_string(parser, NULL, source_code, strlen(source_code));
 
@@ -30,16 +48,16 @@ int main() {
   TSNode array_node = ts_node_named_child(root_node, 0);
   TSNode number_node = ts_node_named_child(array_node, 0);
 
-  // Check that the nodes have the expected types.
-  assert(strcmp(ts_node_type(root_node), "value") == 0);
-  assert(strcmp(ts_node_type(array_node), "array") == 0);
-  assert(strcmp(ts_node_type(number_node), "number") == 0);
+  //// Check that the nodes have the expected types.
+  //assert(strcmp(ts_node_type(root_node), "value") == 0);
+  //assert(strcmp(ts_node_type(array_node), "array") == 0);
+  //assert(strcmp(ts_node_type(number_node), "number") == 0);
 
-  // Check that the nodes have the expected child counts.
-  assert(ts_node_child_count(root_node) == 1);
-  assert(ts_node_child_count(array_node) == 5);
-  assert(ts_node_named_child_count(array_node) == 2);
-  assert(ts_node_child_count(number_node) == 0);
+  //// Check that the nodes have the expected child counts.
+  //assert(ts_node_child_count(root_node) == 1);
+  //assert(ts_node_child_count(array_node) == 5);
+  //assert(ts_node_named_child_count(array_node) == 2);
+  //assert(ts_node_child_count(number_node) == 0);
 
   // Print the syntax tree as an S-expression.
   char *string = ts_node_string(root_node);
